cc3f1c0b5ad90ce3a4bad3338d0f6a26b554c470
Narenboi <73293318+Narenboi@users.noreply.github.com>
2022-12-12 12:09:43 +0100

    Delete Projekt1 fxmln directory

--- Projekt1 fxmln/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.projekt1fxmln {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.projekt1fxmln to javafx.fxml;
-    exports com.example.projekt1fxmln;
-}
